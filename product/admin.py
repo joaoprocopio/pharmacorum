f45c12fb79d4f315d10a90da622bb4b9353e6639
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-06 13:24:22 -0300

    melhora o admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from product.models import Brand, Product, Stock
+
 # Register your models here.
+
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Stock)
--- product/models.py
+++ product/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Brand(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     class ProductTypes(models.TextChoices):
@@ -22,6 +25,13 @@ class Product(models.Model):
     description = models.TextField(max_length=256)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(null=True, blank=True)
+
+    def update(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return f"{self.brand} - {self.title}"
 
 
 class Stock(models.Model):
