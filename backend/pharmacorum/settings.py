81370708a9d7d57ba38ceb0af418c85aa351c210
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-07 12:59:49 -0300

    remove comentário

--- backend/pharmacorum/settings.py
+++ backend/pharmacorum/settings.py
@@ -3,26 +3,16 @@ from pathlib import Path
 from corsheaders.defaults import default_headers, default_methods
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY", default="django-insecure-key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = [
     "localhost",
     "0.0.0.0",
 ]
-
-
-# Application definition
 
 DJANGO_APPS = [
     "django.contrib.admin",
@@ -66,7 +56,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-ROOT_URLCONF = "backend.backend.urls"
+ROOT_URLCONF = "backend.pharmacorum.urls"
 
 TEMPLATES = [
     {
@@ -84,11 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "backend.backend.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+WSGI_APPLICATION = "backend.pharmacorum.wsgi.application"
 
 DATABASES = {
     "default": {
@@ -100,10 +86,6 @@ DATABASES = {
         "PORT": config("DJANGO_DB_PORT", default=5432),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -120,10 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -132,13 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- backend/product/admin.py
+++ backend/product/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from backend.product.models import Brand, Product, Stock
 
-# Register your models here.
-
 admin.site.register(Brand)
 admin.site.register(Product)
 admin.site.register(Stock)
