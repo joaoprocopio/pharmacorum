3fc11205fe5997f07be520e7ced59c32f1570e40
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-07 14:03:22 -0300

    coloca mocks

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -4,6 +4,12 @@ import { createPinia } from "pinia"
 import App from "./App.vue"
 import router from "~/router"
 
+import { env } from "@/configs"
+
+if (env.isDev && env.isMock) {
+  // turn up mock server
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
