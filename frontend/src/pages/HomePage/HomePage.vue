1368e96bfe8adc90a6cc7d24360399fa3f1809a8
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-10 11:32:54 -0300

    gambiarrinha pra testar a mock

--- frontend/src/pages/HomePage/HomePage.vue
+++ frontend/src/pages/HomePage/HomePage.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>THIS IS A HOME PAGE!</div>
+  <div
+    v-for="product in products"
+    :key="product.id">
+    {{ product }}
+  </div>
 </template>
 
 <script setup>
-  import { onMounted } from "vue"
+  import { onMounted, ref } from "vue"
   import { $axios } from "~/api"
 
+  const products = ref({})
+
   onMounted(() => {
-    $axios.get("/api/products/").then((r) => console.log(r.data.products[0]))
+    $axios.get("/api/products/").then((r) => (products.value = r.data.products))
   })
 </script>
