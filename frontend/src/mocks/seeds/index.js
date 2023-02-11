ec33ef084d184a3df4a5e1c46116171261ca8084
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-11 13:12:08 -0300

    nem precisava ser async 🤡

--- frontend/src/mocks/seeds/index.js
+++ frontend/src/mocks/seeds/index.js
@@ -1,7 +1,7 @@
-async function seeds(server) {
-  await server.loadFixtures()
-  await server.createList("brand", 10)
-  await server.createList("product", 50)
+function seeds(server) {
+  server.loadFixtures()
+  server.createList("brand", 10)
+  server.createList("product", 50)
 }
 
 export { seeds }
