b6763d44133d01453159cf3227fb8c9749bc6249
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-13 16:18:02 -0300

    aprendia a serializar

--- frontend/src/mocks/routes/index.js
+++ frontend/src/mocks/routes/index.js
@@ -1,30 +1,36 @@
 import { Response } from "miragejs"
 
 function routes() {
-  this.get("api/users/", (schema) => {
+  this.get("api/users/", function (schema) {
     return schema.users.all()
   })
-  this.get("api/products/", (schema, request) => {
+  this.get("api/products/", function (schema, request) {
     const params = {
       page: parseInt(request?.queryParams?.page) || 1,
       per_page: parseInt(request?.queryParams?.per_page) || 30,
     }
 
-    const response = schema.products
-      .all()
-      .slice(
-        params.page * params.per_page - params.per_page,
-        params.page * params.per_page
-      )
-      .sort((a, b) => {
-        return new Date(b.updatedAt) - new Date(a.updatedAt)
-      })
+    const products = this.serialize(
+      schema.products
+        .all()
+        .slice(
+          params.page * params.per_page - params.per_page,
+          params.page * params.per_page
+        )
+        .sort((a, b) => {
+          return new Date(b.updatedAt) - new Date(a.updatedAt)
+        })
+    )
 
-    if (response.length <= 0) {
+    if (products.length <= 0) {
       return new Response(400, {}, {})
     }
 
-    return new Response(200, {}, response)
+    return new Response(
+      200,
+      {},
+      { response_length: schema.products.all().length, products: products }
+    )
   })
 }
 
--- frontend/src/mocks/serializers/index.js
+++ frontend/src/mocks/serializers/index.js
@@ -3,6 +3,7 @@ import { ActiveModelSerializer } from "miragejs"
 const serializers = {
   product: ActiveModelSerializer.extend({
     embed: true,
+    root: false,
     include: ["brand", "type"],
   }),
   user: ActiveModelSerializer,
