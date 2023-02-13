e9b9413c94dd3f23151e30d46ac6d6165c55d5d1
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-13 13:35:18 -0300

    paginação simples

--- frontend/src/mocks/routes/index.js
+++ frontend/src/mocks/routes/index.js
@@ -1,14 +1,32 @@
+import { Response } from "miragejs"
+
 function routes() {
   this.get("api/users/", (schema) => {
     return schema.users.all()
   })
   this.get("api/products/", (schema, request) => {
-    request.requestBody = request.sendArguments[0]
+    const params = {
+      page: 1,
+      per_page: 30,
+    }
 
-    console.log(JSON.parse(request.requestBody))
-
-    return schema.products.all()
+    return new Response(
+      200,
+      {},
+      schema.products
+        .all()
+        .slice(
+          params.page * params.per_page - params.per_page,
+          params.page * params.per_page
+        )
+    )
   })
 }
 
 export { routes }
+
+// if request.body:
+//         params = loads(request.body)
+
+//     if ("per_page" and "page") not in params.keys():
+//         return JsonResponse({}, status=HTTPStatus.BAD_REQUEST)
