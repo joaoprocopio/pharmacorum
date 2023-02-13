5df71fb39100f874ce78c50e6cd2f4e3e197abe5
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-13 14:34:44 -0300

    começa a implementação de paginação

--- frontend/src/pages/HomePage/HomePage.vue
+++ frontend/src/pages/HomePage/HomePage.vue
@@ -14,17 +14,28 @@
       </div>
     </div>
   </template>
+  <VPagination :length="pages"></VPagination>
 </template>
 
 <script setup>
-  import { onMounted, ref } from "vue"
+  import { onMounted, computed, ref } from "vue"
   import { $axios } from "~/api"
 
+  const page = ref(1)
+  const perPage = ref(30)
+
+  const pages = computed(() => responseLength.value / perPage.value)
+  const responseLength = ref(0)
   const products = ref({})
 
   onMounted(() => {
     $axios
-      .get("/api/products", { params: { per_page: 100 } })
-      .then((response) => (products.value = response.data.products))
+      .get("/api/products", {
+        params: { per_page: perPage.value, page: page.value },
+      })
+      .then((response) => {
+        responseLength.value = response.data.response_length
+        products.value = response.data.products
+      })
   })
 </script>
