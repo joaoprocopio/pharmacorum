84367e035a68584d248ac9e9e8ff3841d4a6dbe2
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-14 13:34:58 -0300

    appbar

---
+++ frontend/src/components/app/AppBar/AppBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <VAppBar
+    :border="true"
+    density="comfortable"
+    class="px-4"
+    elevation="0">
+    <template #prepend>
+      <VBtn
+        icon="spa"
+        variant="plain"
+        :to="{ name: HomePageName }" />
+    </template>
+    <template #append>
+      <VBtn
+        :icon="$globalTheme.theme === 'light' ? 'light_mode' : 'dark_mode'"
+        variant="plain"
+        @click="$globalTheme.toggleTheme" />
+    </template>
+  </VAppBar>
+</template>
+
+<script setup>
+  import { HomePageName } from "~/assets"
+  import { useGlobalThemeStore } from "~/stores"
+
+  const $globalTheme = useGlobalThemeStore()
+</script>
--- frontend/src/layouts/DefaultLayout/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout/DefaultLayout.vue
@@ -1,7 +1,11 @@
 <template>
-  <VAppBar />
+  <AppBar />
   <VMain>
     <slot />
   </VMain>
   <VFooter />
 </template>
+
+<script setup>
+  import { AppBar } from "~/components"
+</script>
