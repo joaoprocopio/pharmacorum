c1aca060c7b390f11e1963992af8d0c91f38ce68
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-22 17:22:28 -0300

    caso não tenha usuário e esteja indo para a tela de autenticação, volta para a tela padrão

--- frontend/src/plugins/router-plugin/sessions-guard.js
+++ frontend/src/plugins/router-plugin/sessions-guard.js
@@ -1,9 +1,13 @@
 import { SessionsServices } from "~/services"
 import { useSessionsStore } from "~/stores"
-import { SessionsPageName, ErrorPageName, ProductsPageName } from "~/assets"
+import {
+  SessionsPageName,
+  ErrorPageName,
+  ProductsPageName,
+  SessionsSteps,
+} from "~/assets"
 
 export const sessionsGuard = async (to, from, next) => {
-  // TODO: criar um guard que redireciona quando tem um usuário válido em $sessions.findUser
   const $sessions = useSessionsStore()
   $sessions.currentUser = await SessionsServices.currentUser()
 
@@ -14,6 +18,13 @@ export const sessionsGuard = async (to, from, next) => {
 
   if (to.name === SessionsPageName && $sessions.isAuthenticated)
     return next({ name: ProductsPageName })
+
+  if (
+    to.name === SessionsPageName &&
+    to.params.step === SessionsSteps.AUTHENTICATION &&
+    !$sessions.findUser?.id
+  )
+    return next({ name: SessionsPageName })
 
   next()
 }
