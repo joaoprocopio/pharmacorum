9c042f6bcff605d181d15c2cb29d6c57a91d9032
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-22 16:45:30 -0300

    mechi no lint

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   semi: false,
-  singleQuote: false,
-  useTabs: false,
   vueIndentScriptAndStyle: true,
-  bracketSpacing: true,
   bracketSameLine: true,
-  singleAttributePerLine: true,
-  arrowParens: "always",
-  trailingComma: "es5",
-  tabWidth: 2,
 }
