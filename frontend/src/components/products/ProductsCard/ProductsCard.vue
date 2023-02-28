0def0a2057ecb34e1d6a48fccbe7d979b5e2d115
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-02-28 20:44:43 -0300

    deixa o card mais semantico

--- frontend/src/components/products/ProductsCard/ProductsCard.vue
+++ frontend/src/components/products/ProductsCard/ProductsCard.vue
@@ -1,33 +1,31 @@
 <template>
-  <VCard
-    border
-    class="mb-8 px-6 py-4"
-    elevation="0"
-    :to="{
-      /* TODO */
-    }">
-    <div class="d-flex justify-space-between mb-4">
-      <div class="d-flex flex-column">
-        <div class="text-h6" v-text="$props.product.title" />
-        <div class="text-grey" v-text="$props.product.brand.name" />
-      </div>
-      <div class="d-flex flex-column align-end">
-        <div class="text-h6" v-text="$props.product.quantity" />
-        <div class="text-grey" v-text="'Available'" />
-      </div>
-    </div>
-    <div class="mb-4" v-text="$props.product.description" />
-    <div class="d-flex justify-space-between align-center">
-      <div>
+  <VCard border class="mb-8 px-6 py-4" elevation="0">
+    <section class="d-flex justify-space-between mb-4">
+      <article class="d-flex flex-column">
+        <h1 v-text="$props.product.title" />
+        <p class="text-grey" v-text="$props.product.brand.name" />
+      </article>
+      <article class="d-flex flex-column align-end">
+        <h1 v-text="$props.product.quantity" />
+        <p class="text-grey" v-text="'Available'" />
+      </article>
+    </section>
+    <section>
+      <article class="mb-4" v-text="$props.product.description" />
+    </section>
+    <section class="d-flex justify-space-between align-center">
+      <article>
         <template v-for="(type, id) in $props.product.types" :key="id">
           <ProductsCardChip
             :title="ProductTypesEnum[type].title"
             :icon="ProductTypesEnum[type].icon"
             :color="ProductTypesEnum[type].color" />
         </template>
-      </div>
-      <div class="ml-2" v-text="currency.format($props.product.price, 0)" />
-    </div>
+      </article>
+      <article class="ml-2">
+        <h1 v-text="currency.format($props.product.price, 0)" />
+      </article>
+    </section>
   </VCard>
 </template>
 
