f5c5f9ab064caa3e70a846ca65cb46c305d9b485
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-03-16 19:33:30 -0300

    migração e modelo

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+
+from backend.products.models import Product
+
+admin.site.register(Product)
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,12 +1,29 @@
-"""
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
-class Product:
-    ...
+from django.utils import timezone
+
+from backend.brands.models import Brand
+
+
+class Product(models.Model):
+    class Types(models.TextChoices):
+        DRUG = ("DRUG", "Drug")
+        SUPPLEMENT = ("SUPPLEMENT", "Supplement")
+        COSMETIC = ("COSMETIC", "Cosmetic")
+        HYGIENE = ("HYGIENE", "Hygiene")
+        BABY_AND_KIDS = ("BABY_AND_KIDS", "Baby and Kids")
+
+    title = models.CharField(max_length=128)
+    types = ArrayField(models.CharField(max_length=32, blank=True, choices=Types.choices), blank=True, default=list)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=32, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    description = models.TextField(max_length=2048)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ["updated_at"]
-        order_with_respect_to = "brand"
-"""
