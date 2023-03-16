16fe2ec05c7f147592d9e00f799e43c330d9af80
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-03-16 16:50:52 -0300

    melhora o teste de paginação

--- backend/brands/tests/test_views.py
+++ backend/brands/tests/test_views.py
@@ -4,10 +4,15 @@ from backend.brands.views import list_brands
 
 
 def test_list_brands_pagination(rf, brands):
-    per_page = 15
-    request = rf.get("/api/brands", {"per_page": per_page})
+    first_request = rf.get("/api/brands", {"page": 1, "per_page": 15})
+    second_request = rf.get("/api/brands", {"page": 2, "per_page": 15})
 
-    response = list_brands(request)
-    response_content = loads(response.content)
+    first_response = list_brands(first_request)
+    second_response = list_brands(second_request)
 
-    assert len(response_content.get("brands")) == per_page
+    first_response_content = loads(first_response.content)
+    second_response_content = loads(second_response.content)
+
+    assert len(first_response_content.get("brands")) == 15
+    assert len(second_response_content.get("brands")) == 15
+    assert first_response_content.get("brands")[0]["id"] != second_response_content.get("brands")[0]["id"]
